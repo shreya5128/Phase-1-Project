6058a2146d4395451f4f40be268e8fe9656d5608
Shreya K S <97331236+shreya5128@users.noreply.github.com>
2022-02-04 22:25:30 +0530

    Add files via upload

---
+++ Phase 2 Projects/Phase2APP_FormFieldsTracking/src/main/java/com/shreya/APP/Dashboard.java
@@ -0,0 +1,31 @@
+package com.shreya.APP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Dashboard")
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        String userId = request.getParameter("userid");
+        if (userId == null) {
+            out.println("No UserId was found in hidden form field.<br>");
+        } else {
+            out.println("UserId found in hidden form field: " + userId + "<br>");
+            
+        }
+        out.println("</body></html>");
+
+	}
+
+}
