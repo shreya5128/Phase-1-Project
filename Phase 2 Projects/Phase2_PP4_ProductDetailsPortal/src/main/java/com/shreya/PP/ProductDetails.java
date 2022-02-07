e901ba679172c69451996fba6185dece7ac76833
Shreya K S <97331236+shreya5128@users.noreply.github.com>
2022-02-07 09:03:57 +0530

    First Commit

---
+++ Phase 2 Projects/Phase2_PP4_ProductDetailsPortal/src/main/java/com/shreya/PP/ProductDetails.java
@@ -0,0 +1,33 @@
+package com.shreya.PP;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/App")
+public class ProductDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response, Object session) throws ServletException, IOException {
+		String pId = request.getParameter("productId");
+		String pName = request.getParameter("productName");
+		String pPrice = request.getParameter("productPrice");
+		
+		HttpSession theSession = request.getSession();
+		
+		
+		
+		theSession.setAttribute("pid", pId);
+		theSession.setAttribute("pname", pName);
+		theSession.setAttribute("pprice", pPrice);
+		
+		response.sendRedirect("display.jsp");
+	}
+}
+	
+
+
